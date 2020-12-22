5d1d3a5897096c4b081a96bcbe1875c09fd68d0e
oleg <olegsh-gusev@mail.ru>
2020-12-23 00:17:28 +0300

    fix

--- django-backend/repository/urls.py
+++ django-backend/repository/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('<str:repository_name>/', views.GetRepositoriesView.as_view()),
+  path('<str:repository_name>', views.GetRepositoriesView.as_view()),
 ]
