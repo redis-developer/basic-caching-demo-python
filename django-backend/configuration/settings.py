8021361303320f2b9ea02defb9adf27ca610eb4b
Ruslan <rvagapov@deliveryweb.ru>
2021-01-11 15:03:24 +0300

    add static dir

--- django-backend/configuration/settings.py
+++ django-backend/configuration/settings.py
@@ -20,6 +20,11 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
