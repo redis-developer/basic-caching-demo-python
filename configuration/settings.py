049c314ad8ef068ea62f8fe65a9c4b598dd27420
Ruslan <rvagapov@deliveryweb.ru>
2021-01-20 17:29:44 +0300

    test emistat

--- configuration/settings.py
+++ configuration/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'l8ftx5(_snb7om^ib$!4g%e+x3ph+eqw$*rtx!dnsf%#*2^r*d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ['127.0.0.1', 'localhost', '.vercel.app'])
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ['127.0.0.1', 'localhost', '.vercel.app', 'a.run.app', '.a.run.app'])
 
 CORS_ORIGIN_ALLOW_ALL = True
 
