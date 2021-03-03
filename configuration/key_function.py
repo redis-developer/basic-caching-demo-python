a4d983ed773d2994292a2eaca2058dc9265f31b7
Ruslan <rvagapov@deliveryweb.ru>
2021-03-03 13:38:51 +0300

    add server and client folders

--- configuration/key_function.py
+++
@@ -1,3 +0,0 @@
-def key_maker(key, key_prefix, version):
-    return key
-
--- repository/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-  path('<str:repository_name>', views.GetRepositoriesView.as_view()),
-]
